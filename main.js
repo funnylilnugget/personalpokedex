a4bcec9b50c63635c7870c83d73d084e6cf46ec5
Kenny Stachovich <kenny@funnylilnugget.com>
2018-11-06 17:44:17 -0500

    added javascript

---
+++ main.js
@@ -0,0 +1,36 @@
+// const POKEMON = [];
+
+// var pokemon1 = "latios";
+// var pokemon2 = "blaziken";
+// var pokemon3 = "gardevoir";
+
+class NuggetsPokemon {
+  constructor(name, hp, attack, defense, abilities) {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.abilities = abilities;
+  }
+}
+
+
+
+function getPokemon(pokemonName) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+
+   let poke = new NuggetsPokemon(
+        data.name,
+        data.stats[5]
+      );
+      console.log(poke);
+    }
+  };
+  xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/" + pokemonName + ".json", true);
+  xhttp.send();
+}
+
+// var pokemonSelection = new NuggetsPokemon(pokemon1, pokemon2, pokemon3)
