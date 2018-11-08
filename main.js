251779c371e6b734e8c1a03b7d47ec4650b4fd3d
Kenny Stachovich <kenny@funnylilnugget.com>
2018-11-07 20:55:47 -0500

    added css animations

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ class NuggetsPokemon {
 
 
 
-function getPokemon(pokemonName) {
+function getPokemon1(pokemonName) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
@@ -29,8 +29,41 @@ function getPokemon(pokemonName) {
       console.log(poke);
     }
   };
-  xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/" + pokemonName + ".json", true);
+  xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/latios.json", true);
   xhttp.send();
 }
 
+function getPokemon2(pokemonName) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+
+   let poke = new NuggetsPokemon(
+        data.name,
+        data.stats[5]
+      );
+      console.log(poke);
+    }
+  };
+  xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/blaziken.json", true);
+  xhttp.send();
+}
+
+function getPokemon3(pokemonName) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+
+   let poke = new NuggetsPokemon(
+        data.name,
+        data.stats[5]
+      );
+      console.log(poke);
+    }
+  };
+  xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/gardevoir.json", true);
+  xhttp.send();
+}
 // var pokemonSelection = new NuggetsPokemon(pokemon1, pokemon2, pokemon3)
